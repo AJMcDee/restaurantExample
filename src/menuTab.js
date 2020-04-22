1cb57a70206b641eda18d6059e85ba60c6d4a274
Anna McDougall <anna.mcdougall@gmail.com>
2020-04-22 13:15:23 +0200

    Remove dist from gitignore

--- src/menuTab.js
+++ src/menuTab.js
@@ -9,26 +9,22 @@ class MenuItem {
 
         insert() {
             const newRow = help.makeElement("tr", "", "", menuTable)
-            console.log(this)
             for (const item in this) {
                 const newCell = help.makeElement("td", "", this[item], newRow)
             }
         }
 }
 
-
+/// Fix this to automatically insert for each new menu item
 export function populateMenuTab(){
 
     help.makeContentElement("h1", "menuTitle", "Menu")
     const menuTable = help.makeContentElement("table", "menuTable", "")
 
-    let item1 = new MenuItem("Tiramisu", "Delicious tiramisu is so delicious!", "$3.99")
-    item1.insert()
+    let item1 = new MenuItem("Tiramisu", "Delicious tiramisu is so delicious!", "$3.99").insert()
 
-    let item2 = new MenuItem("Spaghetti Bolognese", "Mmmm a classic from mama's kitchen", "$9.99")
-    item2.insert()
+    let item2 = new MenuItem("Spaghetti Bolognese", "Mmmm a classic from mama's kitchen", "$9.99").insert()
 
-    let item3 = new MenuItem("Carbonara", "ITSA SO CREAMY", "$11.80")
-    item3.insert()
+    let item3 = new MenuItem("Carbonara", "ITSA SO CREAMY", "$11.80").insert()
 
 }
