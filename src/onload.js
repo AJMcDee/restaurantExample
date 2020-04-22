9774b10ab9d543445d0fd2bc1682e8cb3d8ec605
Anna McDougall <anna.mcdougall@gmail.com>
2020-04-22 13:26:30 +0200

    Check

--- src/onload.js
+++ src/onload.js
@@ -16,7 +16,7 @@ const title = help.makeContentElement("h1", "title", "La Piazza Restaurant")
 const pizzaImg = help.makeContentElement("img", "pizza", "");
 document.getElementById("pizza").src = "https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg";
 
-const promoCopy = "Mamma mia! What a great place to get some pizza! Don't miss out on this incredible experience, with each pizza crafted especially by the Italian masters themselves. Our restaurant not only provides an exceptional culinary experience, but also delivers a romantic setting perfect for that special night out."
+const promoCopy = "Mamma mia!! What a great place to get some pizza! Don't miss out on this incredible experience, with each pizza crafted especially by the Italian masters themselves. Our restaurant not only provides an exceptional culinary experience, but also delivers a romantic setting perfect for that special night out."
 
 const promoText = help.makeContentElement("p", "promoCopy", promoCopy);
 }
